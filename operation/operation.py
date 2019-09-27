93e0c085cc0628ddfa3919c7e666ce5fa5f5ada6
zephyrkul <zephyrkul@users.noreply.github.com>
2019-09-27 10:29:08 -0600

    disband

--- operation/operation.py
+++ operation/operation.py
@@ -58,11 +58,11 @@ def message_format(message, last_message):
     if not last_message:
         final.append(str(message.created_at.date()))
     elif message.created_at.date() != last_message.created_at.date():
-        final.extend(("", str(message.created_at.date())))
+        final.extend(("", message.created_at.date().isoformat()))
     if message.author.bot:
-        author = f"BOT {message.author}"
+        author = f"BOT {message.author.display_name}"
     else:
-        author = message.author
+        author = message.author.display_name
     if message.edited_at:
         if message.edited_at.date() == message.created_at.date():
             post = f" (edited {message.edited_at.time().isoformat('minutes')})"
@@ -91,18 +91,17 @@ async def log(team, destination):
     if len(bios) == 1:
         bios = [discord.File(bios[-1], filename=f"{channel}.md")]
     else:
-        bios = [discord.File(bio, filename=f"{channel}-{i}.md") for i, bio in enumerate(bios)]
+        bios = [discord.File(bio, filename=f"{channel}_part-{i}.md") for i, bio in enumerate(bios)]
     embed = discord.Embed(
         title=str(channel),
         author=f"{team['leader'].top_role} {team['leader']}",
         description="\n".join(f"{m.top_role.mention} {m.mention}" for m in team["soldiers"]),
     )
+    if not destination:
+        destination = team["channel"]
+    await destination.send(embed=embed)
     for bio in bios:
-        if destination:
-            await destination.send(embed=embed, file=bio)
-        else:
-            team["channel"].send(embed=embed, file=bio)
-        embed = None
+        await destination.send(file=bio)
 
 
 class Operation(commands.Cog):
@@ -286,6 +285,9 @@ class Operation(commands.Cog):
             for i, member in enumerate(args[Member]):
                 if not any(role in args[Role] for role in member.roles):
                     return await ctx.send(f"{member} doesn't have any of the required roles.")
+                staging_overs[member] = discord.PermissionOverwrite(
+                    read_messages=False, connect=False
+                )
                 op_overs[i][member] = discord.PermissionOverwrite(
                     read_messages=True, send_messages=True, mention_everyone=True
                 )
@@ -344,9 +346,6 @@ class Operation(commands.Cog):
         # return await ctx.send("Only op leaders can end ops")
         op = self.operations.pop(ctx.guild)
         reason = get_audit_reason(ctx.author, "Operation end.")
-        # TODO: get update information
-        # await asyncio.gather(*(channel.delete(reason=reason) for channel in op["category"].text_channels))
-
         async with ctx.typing():
             archives = ctx.guild.get_channel(await self.config.guild(ctx.guild).op_archive())
             for team in op["teams"]:
@@ -354,8 +353,10 @@ class Operation(commands.Cog):
                 if archives:
                     await team["channel"].delete(reason=reason)
                 else:
-                    await team["channel"].edit(sync_permissions=True)
-            await op["category"].voice_channels[-1].edit(name="🚫", sync_permissions=True)
+                    await team["channel"].edit(sync_permissions=True, reason=reason)
+            await op["category"].voice_channels[-1].edit(
+                name="🚫", sync_permissions=True, reason=reason
+            )
         await ctx.tick()
         # TODO: post update information
 
@@ -378,6 +379,57 @@ class Operation(commands.Cog):
                 await team["channel"].set_permissions(member, overwrite=None)
         await ctx.send(f"Member {member} has been removed from this op.")
 
+    @commands.command()
+    @requires(OFFICER)
+    async def disband(self, ctx, *, leader: Member = None):
+        """
+        Disbands a team led by yourself or the specified leader.
+        """
+        leader = leader or ctx.author
+        if leader != ctx.author:
+            # TODO: check for COMMAND authority
+            pass
+        # get leader's team
+        teams = self.operations[ctx.guild]["teams"]
+        if len(teams) == 1:
+            return await ctx.send(
+                f"There's only one team left. Use `{ctx.prefix}update_over` instead."
+            )
+        for i, team in enumerate(teams):
+            if team["leader"] == leader:
+                break
+        else:
+            return await ctx.send(f"No team found led by {leader}.")
+        teams.pop(i)
+        # distribute leader's team
+        for member in team["soldiers"]:
+            if len(teams) == 1:
+                team = teams[0]
+            else:
+                weights = [len(t.setdefault("soldiers", set())) for t in teams]
+                m = max(weights)
+                weights = [m - w for w in weights]
+                team = random.choices(teams, weights)[0]
+            team.setdefault("soldiers", set()).add(member)
+            overs = team["channel"].overwrites_for(member)
+            overs.update(read_messages=True, send_messages=True)
+            # assign permissions
+            await team["channel"].set_permissions(member, overwrite=overs)
+            await team["channel"].send(f"{member.mention} has joined.")
+        # archive op channel
+        archives = ctx.guild.get_channel(await self.config.guild(ctx.guild).op_archive())
+        await log(team, archives)
+        if archives:
+            await team["channel"].delete()
+        else:
+            await team["channel"].edit(sync_permissions=True)
+        await ctx.tick()
+
+    @commands.command()
+    @requires(OFFICER)
+    async def participants(self, ctx, *, leader: Member = None):
+        leader = leader or ctx.author
+
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
         if member.bot:
@@ -389,7 +441,7 @@ class Operation(commands.Cog):
         op = self.operations[member.guild]
         if after.channel != op["staging"]:
             return
-        if member in op.get("blacklist", set()):
+        if member in op.setdefault("blacklist", set()):
             return
         # assign member
         teams = op["teams"]
